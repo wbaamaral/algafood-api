0e56ea2583739425ede26616c5f4fe4e539b8fd5
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2022-10-10 23:14:44 -0400

    Modificação massa de dados para testes

--- src/main/resources/db/devdata/afterMigrate.sql
+++ src/main/resources/db/devdata/afterMigrate.sql
@@ -1,3 +1,18 @@
+/*
+ * Arquivo de massa de dados para testes
+ * para uso em ambiente de desenvolvimento
+ * utiliza callback do Flyway rodando após migrações
+ * 
+ */
+
+use algafood;
+
+/*
+ * 
+ * Limpar dados
+ * 
+ */
+
 set foreign_key_checks = 0;
 
 delete from cidade;
@@ -15,6 +30,12 @@ delete from usuario_grupo;
 
 set foreign_key_checks = 1;
 
+/*
+ * 
+ * Reset no auto increment
+ * 
+ */
+
 alter table cidade auto_increment = 1;
 alter table cozinha auto_increment = 1;
 alter table estado auto_increment = 1;
@@ -25,20 +46,36 @@ alter table produto auto_increment = 1;
 alter table restaurante auto_increment = 1;
 alter table usuario auto_increment = 1;
 
+
+/*
+ * 
+ * Inserindo Dados de teste
+ * 
+ */
 insert into cozinha (id, nome) values (1, 'Tailandesa');
 insert into cozinha (id, nome) values (2, 'Indiana');
 insert into cozinha (id, nome) values (3, 'Argentina');
 insert into cozinha (id, nome) values (4, 'Brasileira');
+insert into cozinha (id, nome) values (5, 'Coreana');
 
 insert into estado (id, nome) values (1, 'Minas Gerais');
 insert into estado (id, nome) values (2, 'São Paulo');
 insert into estado (id, nome) values (3, 'Ceará');
+insert into estado (id, nome) values (4, 'Rondônia');
+insert into estado (id, nome) values (5, 'Acre');
+insert into estado (id, nome) values (6, 'Bahia');
+insert into estado (id, nome) values (8, 'Distrito Federal');
+insert into estado (id, nome) values (9, 'Amazonas');
 
 insert into cidade (id, nome, estado_id) values (1, 'Uberlândia', 1);
 insert into cidade (id, nome, estado_id) values (2, 'Belo Horizonte', 1);
 insert into cidade (id, nome, estado_id) values (3, 'São Paulo', 2);
 insert into cidade (id, nome, estado_id) values (4, 'Campinas', 2);
 insert into cidade (id, nome, estado_id) values (5, 'Fortaleza', 3);
+insert into cidade (id, nome, estado_id) values (6, 'Porto Velho', 4);
+insert into cidade (id, nome, estado_id) values (7, 'Ariquemes', 4);
+insert into cidade (id, nome, estado_id) values (8, 'Jaru', 4);
+insert into cidade (id, nome, estado_id) values (9, 'Ji-Paraná', 4);
 
 insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values (1, 'Thai Gourmet', 10, 1, utc_timestamp, utc_timestamp, 1, '38400-999', 'Rua João Pinheiro', '1000', 'Centro');
 insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (2, 'Thai Delivery', 9.50, 1, utc_timestamp, utc_timestamp);
@@ -47,9 +84,11 @@ insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_a
 insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (5, 'Lanchonete do Tio Sam', 11, 4, utc_timestamp, utc_timestamp);
 insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao) values (6, 'Bar da Maria', 6, 4, utc_timestamp, utc_timestamp);
 
+
 insert into forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
 insert into forma_pagamento (id, descricao) values (2, 'Cartão de débito');
 insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
+insert into forma_pagamento (id, descricao) values (4, 'Pix');
 
 insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
 insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
@@ -61,6 +100,7 @@ insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Cam
 
 insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Salada picante com carne grelhada', 'Salada de folhas com cortes finos de carne bovina grelhada e nosso molho especial de pimenta vermelha', 87.20, 1, 2);
 
+
 insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Garlic Naan', 'Pão tradicional indiano com cobertura de alho', 21, 1, 3);
 insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Murg Curry', 'Cubos de frango preparados com molho curry e especiarias', 43, 1, 3);
 
@@ -70,3 +110,5 @@ insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('T-B
 insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Sanduíche X-Tudo', 'Sandubão com muito queijo, hamburger bovino, bacon, ovo, salada e maionese', 19, 1, 5);
 
 insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Espetinho de Cupim', 'Acompanha farinha, mandioca e vinagrete', 8, 1, 6);
+
+
