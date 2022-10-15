a1b9ec91c66dcce42e263b4639d0cbaf1bda81a1
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2022-10-14 22:45:52 -0400

    Refatorando Cadastro Cidade

--- src/main/java/br/com/wbaamaral/algafoodapi/domain/service/CadastroCidadeService.java
+++ src/main/java/br/com/wbaamaral/algafoodapi/domain/service/CadastroCidadeService.java
@@ -15,20 +15,20 @@ import br.com.wbaamaral.algafoodapi.domain.repository.EstadoRepository;
 @Service
 public class CadastroCidadeService {
 
+	private static final String MSG_CIDADE_EM_USO = "Cidade de código %d não pode ser removida, pois está em uso";
+
+	private static final String MSG_CIDADE_NAO_ENCONTRADA = "Não existe um cadastro de cidade com código %d";
+
 	@Autowired
 	private CidadeRepository cidadeRepository;
 	
 	@Autowired
-	private EstadoRepository estadoRepository;
+	private CadastroEstadoService cadastroEstado;
 	
 	public Cidade salvar(Cidade cidade) {
 		Long estadoId = cidade.getEstado().getId();
-
-		Estado estado = estadoRepository.findById(estadoId)
-			.orElseThrow(() -> new EntidadeNaoEncontradaException(
-					String.format("Não existe cadastro de estado com código %d", estadoId)));
 		
-		cidade.setEstado(estado);
+		cidade.setEstado(cadastroEstado.buscarOuFalhar(estadoId));
 		
 		return cidadeRepository.save(cidade);
 	}
@@ -39,11 +39,11 @@ public class CadastroCidadeService {
 			
 		} catch (EmptyResultDataAccessException e) {
 			throw new EntidadeNaoEncontradaException(
-				String.format("Não existe um cadastro de cidade com código %d", cidadeId));
+				String.format(MSG_CIDADE_NAO_ENCONTRADA, cidadeId));
 		
 		} catch (DataIntegrityViolationException e) {
 			throw new EntidadeEmUsoException(
-				String.format("Cidade de código %d não pode ser removida, pois está em uso", cidadeId));
+				String.format(MSG_CIDADE_EM_USO, cidadeId));
 		}
 	}
 	
