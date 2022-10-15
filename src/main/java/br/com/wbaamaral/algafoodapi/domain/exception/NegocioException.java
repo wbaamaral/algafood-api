e1319d0fc985a6f056b3ab6ba898ffa6ed8140ae
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2022-10-15 12:16:25 -0400

    Adicionando Negocio Exception

---
+++ src/main/java/br/com/wbaamaral/algafoodapi/domain/exception/NegocioException.java
@@ -0,0 +1,14 @@
+package br.com.wbaamaral.algafoodapi.domain.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+public class NegocioException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public NegocioException(String mensagem) {
+		super(mensagem);
+	}
+}
