80f0e101314ecfc669106d6b85dbd639565fda42
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2022-10-22 18:09:47 -0400

    8.21. Tratando a exception InvalidFormatException na desserialização

--- README.md
+++ README.md
@@ -185,7 +185,7 @@ Estudo de apis rest, Wélyqrson Bastos Amaral.
     8.18. Padronizando o formato de problemas no corpo de respostas com a RFC 7807 :feet:\
     8.19. Desafio: usando o formato de problemas no corpo de respostas :feet:\
     8.20. Customizando exception handlers de ResponseEntityExceptionHandler :feet:\
-    8.21. Tratando a exception InvalidFormatException na desserialização \
+    8.21. Tratando a exception InvalidFormatException na desserialização :feet:\
     8.22. Habilitando erros na desserialização de propriedades inexistentes ou ignoradas \
     8.23. Desafio: tratando a PropertyBindingException na desserialização   \
     8.24. Lançando exception de desserialização na atualização parcial (PATCH) \
--- src/main/java/br/com/wbaamaral/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/br/com/wbaamaral/algafoodapi/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,5 +1,8 @@
 package br.com.wbaamaral.algafoodapi.api.exceptionhandler;
 
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 
 import br.com.wbaamaral.algafoodapi.domain.exception.EntidadeEmUsoException;
 import br.com.wbaamaral.algafoodapi.domain.exception.EntidadeNaoEncontradaException;
@@ -23,9 +28,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		ProblemType problemType = ProblemType.ENTIDADE_NAO_ENCONTRADA;
 		String detail = ex.getMessage();
-		
+
 		Problem problem = createProblemBuilder(status, problemType, detail).build();
-		
+
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
 
@@ -35,9 +40,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		HttpStatus status = HttpStatus.CONFLICT;
 		ProblemType problemType = ProblemType.ENTIDADE_EM_USO;
 		String detail = ex.getMessage();
-		
+
 		Problem problem = createProblemBuilder(status, problemType, detail).build();
-		
+
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
 
@@ -47,9 +52,9 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		ProblemType problemType = ProblemType.ERRO_NEGOCIO;
 		String detail = ex.getMessage();
-		
+
 		Problem problem = createProblemBuilder(status, problemType, detail).build();
-		
+
 		return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
 	}
 
@@ -58,45 +63,57 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 			HttpStatus status, WebRequest request) {
 
 		if (body == null) {
-			body = Problem.builder()
-					.title(status.getReasonPhrase())
-					.status(status.value())
-					.build();
+			body = Problem.builder().title(status.getReasonPhrase()).status(status.value()).build();
+			
 		} else if (body instanceof String) {
-			body = Problem.builder()
-					.title((String) body)
-					.status(status.value())
-					.build();
+			body = Problem.builder().title((String) body).status(status.value()).build();
+			
 		}
 
 		return super.handleExceptionInternal(ex, body, headers, status, request);
 	}
 
-		
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		Throwable rootCause = ExceptionUtils.getRootCause(ex);
+
+		if (rootCause instanceof InvalidFormatException) {
+			return handleIvalidFormatException((InvalidFormatException) rootCause, headers, status, request);
+		}
+
 		ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+		
 		String detail = "O corpo da requisição está inválido. Verifique erro de sintaxe.";
-		
+
 		Problem problem = createProblemBuilder(status, problemType, detail).build();
-		
-		/*
-		 *  Na aula ficou passando uma nova instância de HttpHeaders, mas o ideal
-		 *  é repassar o "headers" que recebemos como argumento do método
-		 *  return handleExceptionInternal(ex, problem, new HttpHeaders(), status, request);
-		 *  
-		 */ 
-		
+
 		return handleExceptionInternal(ex, problem, headers, status, request);
 	}
 
-	private Problem.ProblemBuilder createProblemBuilder (HttpStatus status, ProblemType problemType, String detail){
+	private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType problemType, String detail) {
 
-		return Problem.builder()
-				.status(status.value())
-				.type(problemType.getUri())
-				.title(problemType.getTitle())
+		return Problem.builder().status(status.value()).type(problemType.getUri()).title(problemType.getTitle())
 				.detail(detail);
 	}
+
+	private ResponseEntity<Object> handleIvalidFormatException(InvalidFormatException ex, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
+
+		// buscar nome da propriedade
+		String path = ex.getPath().stream().map(ref -> ref.getFieldName()).collect(Collectors.joining("."));
+
+		ProblemType problemType = ProblemType.MENSAGEM_INCOMPREENSIVEL;
+		
+		String detail = String.format("A propriedade '%s', "
+				+ "recebeu um valor '%s' que é um tipo inválido. Corrija e informe um valor compativel com o tipo %s.",
+				path, ex.getValue(), ex.getTargetType().getSimpleName());
+
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		
+		return handleExceptionInternal(ex, problem, headers, status, request);
+		
+	}
+
 }
