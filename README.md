6b3849bbbd79306f3ca61cdfae9331a460872c23
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2022-10-22 22:37:58 -0400

    8.24. Lançando exception de desserialização na atualização parcial (PATCH)

--- README.md
+++ README.md
@@ -188,7 +188,7 @@ Estudo de apis rest, Wélyqrson Bastos Amaral.
     8.21. Tratando a exception InvalidFormatException na desserialização :feet:\
     8.22. Habilitando erros na desserialização de propriedades inexistentes ou ignoradas :feet:\
     8.23. Desafio: tratando a PropertyBindingException na desserialização   :feet:\
-    8.24. Lançando exception de desserialização na atualização parcial (PATCH) \
+    8.24. Lançando exception de desserialização na atualização parcial (PATCH) :feet:\
     8.25. Desafio: tratando exception de parâmetro de URL inválido \
     8.26. Desafio: tratando a exceção NoHandlerFoundException \
     8.27. Desafio: tratando outras exceções não capturadas \
--- src/main/java/br/com/wbaamaral/algafoodapi/api/controller/RestauranteController.java
+++ src/main/java/br/com/wbaamaral/algafoodapi/api/controller/RestauranteController.java
@@ -4,9 +4,14 @@ import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.wbaamaral.algafoodapi.domain.exception.CozinhaNaoEncontradaException;
@@ -74,31 +80,37 @@ public class RestauranteController {
 	}
 
 	@PatchMapping("/{restauranteId}")
-	public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos) {
+	public Restaurante atualizarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos,
+			HttpServletRequest request) {
+
+		Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
+
+		merge(campos, restauranteAtual, request);
+
+		return atualizar(restauranteId, restauranteAtual);
+	}
+
+	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino, HttpServletRequest request) {
+		ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
 
 		try {
-			Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+			objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
 
-			merge(campos, restauranteAtual);
+			Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
 
-			return atualizar(restauranteId, restauranteAtual);
-		} catch (CozinhaNaoEncontradaException e) {
-			throw new NegocioException(e.getMessage());
+			dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+				Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+				field.setAccessible(true);
+
+				Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+				ReflectionUtils.setField(field, restauranteDestino, novoValor);
+			});
+		} catch (IllegalArgumentException e) {
+			Throwable rootCause = ExceptionUtils.getRootCause(e);
+			throw new HttpMessageNotReadableException(e.getMessage(), rootCause, serverHttpRequest);
 		}
 	}
-
-	private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
-		ObjectMapper objectMapper = new ObjectMapper();
-		Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
-
-		dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-			Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-			field.setAccessible(true);
-
-			Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
-
-			ReflectionUtils.setField(field, restauranteDestino, novoValor);
-		});
-	}
-
 }
