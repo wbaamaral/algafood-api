644337f0d12a5d84ea26575d561cbe315ac9abf7
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2022-12-10 14:13:05 -0400

    10.16. Desafio: refatorando o código de testes

--- src/test/java/br/com/wbaamaral/algafoodapi/CadastroCozinhaIntegrationTestsIT.java
+++ src/test/java/br/com/wbaamaral/algafoodapi/CadastroCozinhaIntegrationTestsIT.java
@@ -1,7 +1,7 @@
 package br.com.wbaamaral.algafoodapi;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -16,15 +16,21 @@ import org.springframework.test.context.TestPropertySource;
 import br.com.wbaamaral.algafoodapi.domain.model.Cozinha;
 import br.com.wbaamaral.algafoodapi.domain.repository.CozinhaRepository;
 import br.com.wbaamaral.algafoodapi.util.DatabaseCleaner;
+import br.com.wbaamaral.algafoodapi.util.ResourceUtils;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import io.restassured.internal.http.ContentTypeSubTypeExtractor;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @TestPropertySource("/application-test.properties")
 public class CadastroCozinhaIntegrationTestsIT {
 
   private final String BASE_URI = "/cozinhas";
+  private final String[] NOMECOZINHA = { "Tailandesa", "Americana" };
+  private static final int COZINHA_ID_INEXISTENTE = 100;
+
+  private Cozinha cozinhaAmericana;
+  private int quantidadeCozinhasCadastradas;
+  private String jsonCorretoCozinhaChinesa;
 
   @LocalServerPort
   private int port;
@@ -42,18 +48,24 @@ public class CadastroCozinhaIntegrationTestsIT {
     RestAssured.basePath = BASE_URI;
     RestAssured.port = port;
 
+    quantidadeCozinhasCadastradas = NOMECOZINHA.length;
+    jsonCorretoCozinhaChinesa = ResourceUtils.getContentFromResource(
+        "/json/correto/cozinha-chinesa.json");
     dataBaseCleaner.clearTables();
     prepararDados();
+
   }
 
   private void prepararDados() {
-    Cozinha cozinha1 = new Cozinha();
-    cozinha1.setNome("Tailandesa");
-    cozinhaRepository.save(cozinha1);
+    Cozinha cozinhaTailandesa = new Cozinha();
+    cozinhaTailandesa.setNome(NOMECOZINHA[0]);
+    cozinhaRepository.save(cozinhaTailandesa);
 
-    Cozinha cozinha2 = new Cozinha();
-    cozinha2.setNome("Americana");
-    cozinhaRepository.save(cozinha2);
+    cozinhaAmericana = new Cozinha();
+    cozinhaAmericana.setNome(NOMECOZINHA[1]);
+    cozinhaRepository.save(cozinhaAmericana);
+
+    quantidadeCozinhasCadastradas = (int) cozinhaRepository.count();
   }
 
   @Test
@@ -71,47 +83,48 @@ public class CadastroCozinhaIntegrationTestsIT {
   }
 
   @Test
-  public void deveConter4Cozinhas_QaundoConsultarCozinhas() {
+  public void deveRetornarQuantidadeCorretaDeCozinhas_QuandoConsultarCozinhas() {
 
     given()
         .accept(ContentType.JSON)
         .when()
         .get()
         .then()
-        .body("", hasSize(2));
+        .body("", hasSize(quantidadeCozinhasCadastradas));
 
   }
 
   @Test
-  public void testRetornarStatus201_QuandoCadastrarCozinha() {
+  public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+
     given()
-        .body("{ \"nome\": \"Chinesa\" }")
+        .body(jsonCorretoCozinhaChinesa)
         .contentType(ContentType.JSON)
         .accept(ContentType.JSON)
         .when()
         .post()
         .then()
         .statusCode(HttpStatus.CREATED.value());
+
   }
 
   @Test
   public void deveRetornarRespostaEStatusCorretos_QuandoConsultarCozinhaExistente() {
-
     given()
-        .pathParam("cozinhaId", 2)
+        .pathParam("cozinhaId", cozinhaAmericana.getId())
+        .accept(ContentType.JSON)
         .when()
         .get("/{cozinhaId}")
         .then()
         .statusCode(HttpStatus.OK.value())
-        .body("nome", equalTo("Americana"));
-
+        .body("nome", equalTo(cozinhaAmericana.getNome()));
   }
 
   @Test
   public void deveRetornarStatus404_QuandoConsultarCozinhaInexistente() {
 
     given()
-        .pathParam("cozinhaId", 100)
+        .pathParam("cozinhaId", COZINHA_ID_INEXISTENTE)
         .accept(ContentType.JSON)
         .when()
         .get("/{cozinhaId}")
