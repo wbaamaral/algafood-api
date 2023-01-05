73f26a7ade15756710016eb15fc6a791401eabb3
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2023-01-05 11:17:07 -0400

    13.20. Preenchendo um relatório JasperReports com JavaBeans e gerando bytes do PDF

--- src/main/java/br/com/wbaamaral/algafoodapi/api/controller/EstatisticasController.java
+++ src/main/java/br/com/wbaamaral/algafoodapi/api/controller/EstatisticasController.java
@@ -1,4 +1,5 @@
 package br.com.wbaamaral.algafoodapi.api.controller;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,31 +20,29 @@ import br.com.wbaamaral.algafoodapi.domain.service.VendaReportService;
 @RequestMapping(path = "/estatisticas")
 public class EstatisticasController {
 
-	@Autowired
-	private VendaQueryService vendaQueryService;
-	
-	@Autowired
-	private VendaReportService vendaReportService;
-	
-	@GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro,
-			@RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
-		return vendaQueryService.consultarVendasDiarias(filtro, timeOffset);
-	}
-	
-	@GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_PDF_VALUE)
-	public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter filtro,
-			@RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
-		
-		byte[] bytesPdf = vendaReportService.emitirVendasDiarias(filtro, timeOffset);
-		
-		var headers = new HttpHeaders();
-		headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=vendas-diarias.pdf");
-		
-		return ResponseEntity.ok()
-				.contentType(MediaType.APPLICATION_PDF)
-				.headers(headers)
-				.body(bytesPdf);
-	}
-	
+   @Autowired
+   private VendaQueryService vendaQueryService;
+
+   @Autowired
+   private VendaReportService vendaReportService;
+
+   @GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_JSON_VALUE)
+   public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro,
+         @RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
+
+      return vendaQueryService.consultarVendasDiarias(filtro, timeOffset);
+   }
+
+   @GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_PDF_VALUE)
+   public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter filtro,
+         @RequestParam(required = false, defaultValue = "+00:00") String timeOffset) {
+
+      byte[] bytesPdf = vendaReportService.emitirVendasDiarias(filtro, timeOffset);
+
+      var headers = new HttpHeaders();
+      headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=vendas-diarias.pdf");
+
+      return ResponseEntity.ok().contentType(MediaType.APPLICATION_PDF).headers(headers).body(bytesPdf);
+   }
+
 }
