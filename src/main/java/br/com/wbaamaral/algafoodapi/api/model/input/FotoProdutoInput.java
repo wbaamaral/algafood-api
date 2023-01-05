6b7653110d00343069d65cb098695fa56b8cd0f3
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2023-01-05 18:41:54 -0400

    14.2. Implementando upload de arquivo com multipart/form-data - A

---
+++ src/main/java/br/com/wbaamaral/algafoodapi/api/model/input/FotoProdutoInput.java
@@ -0,0 +1,15 @@
+package br.com.wbaamaral.algafoodapi.api.model.input;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FotoProdutoInput {
+
+	private MultipartFile arquivo;
+	private String descricao;
+	
+}
