5ee125d35c6515c378b3e77a170e83a6e624cd85
Wélyqrson de Bastos Amaral <welyqrson@gmail.com>
2023-01-19 21:39:02 -0400

    15.13. Reagindo a Domain Events em fases específicas da transação

--- src/main/java/br/com/wbaamaral/algafoodapi/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
+++ src/main/java/br/com/wbaamaral/algafoodapi/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -3,6 +3,7 @@ package br.com.wbaamaral.algafoodapi.domain.listener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import br.com.wbaamaral.algafoodapi.domain.event.PedidoConfirmadoEvent;
 import br.com.wbaamaral.algafoodapi.domain.model.Pedido;
@@ -15,7 +16,17 @@ public class NotificacaoClientePedidoConfirmadoListener {
 	@Autowired
 	private EnvioEmailService envioEmail;
 
-	@EventListener
+	/*
+	 * a mudança da anotação @EventListener para 
+	 * @TransactionalEventListener, determina em que momento o evento será 
+	 * chamado, vamos deixar para depois do commit... isso dependerá da 
+	 * regra de negócio... com @EventListener pode ocorre um caso onde 
+	 * o evento foi disparado, porém o pedido não foi devidamente confirmado.
+	 * @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+	 * o default é after commit
+	 *
+	 */	
+	@TransactionalEventListener()
 	public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
 		Pedido pedido = event.getPedido();
 
--- src/main/java/br/com/wbaamaral/algafoodapi/domain/listener/PontuarClienteFidelidadePedidoConfirmadoListener.java
+++ src/main/java/br/com/wbaamaral/algafoodapi/domain/listener/PontuarClienteFidelidadePedidoConfirmadoListener.java
@@ -2,14 +2,26 @@ package br.com.wbaamaral.algafoodapi.domain.listener;
 
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import br.com.wbaamaral.algafoodapi.domain.event.PedidoConfirmadoEvent;
 
 @Component
 public class PontuarClienteFidelidadePedidoConfirmadoListener {
 
-	//classe ilustrativa das possibilidades de uso do evento.
-	@EventListener
+	/*
+	 * Classe ilustrativa das possibilidades de uso do evento.
+	 * a mudança da anotação @EventListener para 
+	 * @TransactionalEventListener, determina em que momento o evento será 
+	 * chamado, vamos deixar para depois do commit... isso dependerá da 
+	 * regra de negócio... com @EventListener pode ocorre um caso onde 
+	 * o evento foi disparado, porém o pedido não foi devidamente confirmado.
+	 * @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
+	 * o default é after commit
+	 *
+	 */	
+	@TransactionalEventListener()
 	public void pontuarAoConfirmarPedido(PedidoConfirmadoEvent event) {
 		var nomeCliente = event.getPedido().getCliente().getNome();
 		System.out.println("\n\nRecalculando pontuação para " + nomeCliente + "\n\n");
